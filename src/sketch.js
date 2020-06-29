cbef096bacce72594f21c2f9e853079383710826
Pupye <fullhouseya89@gmail.com>
2020-06-29 10:50:55 +0600

    working game
    1 lower frame rate
    2 compute next state
    3 optimized swap

--- src/sketch.js
+++ src/sketch.js
@@ -2,14 +2,17 @@
 
 
 let grid;
+let nextState;
 let width = 400;
 let height = 400;
-let resolution = 20;
+let resolution = 10;
 let running = false;
 
 function setup() {
     createCanvas(width, height); // this is hard coded
     grid = createCellGrid(width / resolution, height / resolution, resolution);
+    nextState = createCellGrid(width / resolution, height / resolution, resolution);
+    frameRate(5);
     buttonStart = createButton('start');
     buttonStart.mousePressed(() => {
         running = true;
@@ -31,6 +34,54 @@ function draw() {
             cell.show();
         })
     });
+    if (running) {
+        computeNextState(grid, nextState);
+        //swap
+        temp = grid;
+        grid = nextState;
+        nextState = temp;
+    }
+}
+
+function computeNextState(grid, next) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let count = countNeighbor(i, j, grid);
+            if (grid[i][j].isAlive == 1) {
+                if (count < 2) {
+                    next[i][j].setLife(0);
+                } else if (count < 4) {
+                    next[i][j].setLife(1);
+                } else {
+                    next[i][j].setLife(0);
+                }
+            } else {
+                if (count == 3) {
+                    next[i][j].setLife(1);
+                } else {
+                    next[i][j].setLife(0);
+                }
+            }
+        }
+    }
+}
+
+function countNeighbor(i, j, grid) {
+    let rows = grid.length;
+    let cols = grid[0].length;
+    let x_dir = [0, -1, -1, 0, 1, 1, -1, 1];
+    let y_dir = [-1, 0, -1, 1, 0, 1, 1, -1];
+    let result = 0;
+    for (let k = 0; k < 8; k++) {
+        let x = i + x_dir[k];
+        let y = j + y_dir[k];
+        x = (x + rows) % rows;
+        y = (y + cols) % cols;
+        result += grid[x][y].isAlive;
+    }
+    return result;
 }
 
 function createCellGrid(rows, cols, dim) {
